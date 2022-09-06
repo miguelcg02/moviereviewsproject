088caa39202af2e0cd3e5024f6815dd1c3461883
Miguel Angel Calvache Giraldo <calvache116@gmail.com>
2022-09-06 10:00:47 -0500

    Visualizacion de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {'name': 'Miguel Angel Calvache Giraldo '}) # Render para apuntar a templates
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies}) # Render para apuntar a templates
 
 def about(request):
     return render(request, 'about.html')
