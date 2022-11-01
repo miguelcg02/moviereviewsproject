9a107d3e19aab2fa317bceae3cf7d7c0d4942cb7
Miguel Angel Calvache Giraldo <calvache116@gmail.com>
2022-11-01 09:25:19 -0500

    Se agrego la IP del deplyment

--- moviereviewsproject/settings.py
+++ moviereviewsproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-w)y1@3-rnaitsoltpu)v5r!@2f3h+-&4#9y$s8co9gg7=n_=)j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['35.239.24.213']
 
 
 # Application definition
